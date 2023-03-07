4a4c55fc9c5533e45dcd0f39bdd29330497e4b97
Thomas-6666 <72489077+Thomas-6666@users.noreply.github.com>
2023-03-07 09:26:03 +0100

    recup data en log

--- views/Index.js
+++ views/Index.js
@@ -1,23 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, TextInput, View, Pressable } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function Index({navigation}) {
-    navigueVille = () => {
-        console.log("test ville");
-        navigation.navigate("Infos");
+  const [ville, setVille] = useState('');
+  const [coords, setCoords] = useState('');
+
+  useEffect(() => {
+    const localiseCoords = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      let latitude = location.coords.latitude;
+      let longitude = location.coords.longitude;
+      setCoords(`${latitude},${longitude}`);
     }
-    navigueGPS = () => {
-        console.log("test gps");
-        navigation.navigate("Infos");
-    }
+
+    localiseCoords();
+  }, []);
+
+  const navigueVille = () => {
+    console.log(ville);
+    navigation.navigate("Infos");
+  }
+  
+  const navigueGPS = async () => {
+    await localiseCoords();
+    console.log(coords);
+    navigation.navigate("Infos");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{"Application\nmétéo GPS"}</Text>
-      <TextInput multiline style={styles.input} placeholder="Ville"/>
-      <Pressable onPress={this.navigueVille} style={styles.btnvalidate}>
+      <TextInput multiline style={styles.input} placeholder="Ville" value={ville} onChangeText={text => setVille(text)}/>
+      <Pressable onPress={navigueVille} style={styles.btnvalidate}>
         <Text style={styles.textbtn}>Voir météo</Text>
       </Pressable>
-      <Pressable onPress={this.navigueGPS} style={styles.btncoords}>
+      <Pressable onPress={navigueGPS} style={styles.btncoords}>
         <Text style={styles.textbtn}>Détecter coordonnées GPS</Text>
       </Pressable>
       <StatusBar style="auto"/>
